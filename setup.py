b148ce0d4b1e9b1a0f9489a9a6ad8e39c48b4355
FranPandol <pandol.francisco@gmail.com>
2021-06-25 02:13:18 -0300

    Test setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 @Date    : 2020-12-18
 @Author  : liyachao
 """
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
 
 # 第三方依赖
 requires = [
@@ -18,26 +18,27 @@ requires = [
     "requests>=2.25.1",
     'click>=7.1.2',
     'coloredlogs>=3.3.2',
-
 ]
-setup(name='py_ios_device',
-      version="2.1.1",
-      description='Get ios data and operate ios devices',
-      author='chenpeijie & liyachao',
-      author_email='cpjsf@163.com',
-      maintainer='chenpeijie & liyachao',
-      maintainer_email='',
-      url='https://github.com/YueChen-C/py-ios-device',
-      packages=find_packages(),
-      long_description=open('README.md').read(),
-      long_description_content_type="text/markdown",
-      platforms=["any"],
-      install_requires=requires,  # 第三方库依赖
-      entry_points={
-          'console_scripts':{
-              'pyidevice=ios_device.main:cli'
-          }
-      },
-      zip_safe=False,
-      include_package_data=True
-      )
+
+setup(
+    name='py_ios_device',
+    version="2.1.1",
+    description='Get ios data and operate ios devices',
+    author='chenpeijie & liyachao',
+    author_email='cpjsf@163.com',
+    maintainer='chenpeijie & liyachao',
+    maintainer_email='',
+    url='https://github.com/YueChen-C/py-ios-device',
+    packages=find_packages(),
+    long_description=open('README.md', encoding='UTF-8').read(),
+    long_description_content_type="text/markdown",
+    platforms=["any"],
+    install_requires=requires,  # 第三方库依赖
+    entry_points={
+        'console_scripts':{
+          'pyidevice=ios_device.main:cli'
+        }
+    },
+    zip_safe=False,
+    include_package_data=True
+)
