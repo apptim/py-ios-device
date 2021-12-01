e52644d3875d1708283cd0274ca2a763eff73042
FranPandol <pandol.francisco@gmail.com>
2021-12-01 15:41:29 -0300

    Bump version to 2.2.4

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ requires = [
     'coloredlogs>=3.3.2',
 ]
 setup(name='py_ios_device',
-      version="2.2.3",
+      version="2.2.4",
       description='Get ios data and operate ios devices',
       author='chenpeijie & liyachao',
       author_email='cpjsf@163.com',
